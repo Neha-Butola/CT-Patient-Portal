5c8a3a918b7b3ea0d03e29d9412a21484faa480b
bhartesh <bharteshsingh11@gmail.com>
2021-09-05 15:26:02 +0530

    medication and allergies screes

---
+++ src/app/medication-and-allergies/medication-and-allergies-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MedicationDetailsComponent } from './components/medication-details/medication-details.component';
+import { MedicationFormComponent } from './components/medication-form/medication-form.component';
+import { MedicationHistoryComponent } from './components/medication-history/medication-history.component';
+
+const routes: Routes = [
+  {
+    path: 'form',
+    component: MedicationFormComponent,
+  },
+  {
+    path: 'history',
+    component: MedicationHistoryComponent,
+  },
+  {
+    path: 'details',
+    component: MedicationDetailsComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class MedicationAndAllergiesRoutingModule {}
