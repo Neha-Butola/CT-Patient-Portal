be0ed91ef516dcf0517cf526a930ffca2989af29
sitikanta7 <sitikanta42@gmail.com>
2021-09-22 15:43:21 +0530

    Admin panel created with role based access

--- src/app/dashboard/admin-dashboard/admin-dashboard.module.ts
+++ src/app/dashboard/admin-dashboard/admin-dashboard.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminDashboardRoutingModule } from './admin-dashboard-routing.module';
-
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { CoreModule } from 'src/app/core/core.module';
+import { MaterialModule } from 'src/app/material.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [AdminDashboardComponent],
   imports: [
     CommonModule,
-    AdminDashboardRoutingModule
-  ]
+    CoreModule,
+    MaterialModule,
+    AdminDashboardRoutingModule,
+  ],
 })
-export class AdminDashboardModule { }
+export class AdminDashboardModule {}
